cb5dd224b35757bec57686794ab0cf1b08873945
123ZOKzok <zippyondiba@gmail.com>
2024-07-04 14:29:44 +0300

    Define views and API endpoints

--- crowdfunding_platform/projects/views.py
+++ crowdfunding_platform/projects/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+# projects/views.py
+from rest_framework import viewsets # type: ignore
+from .models import Project, Donation
+from .serializers import ProjectSerializer, DonationSerializer
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+class DonationViewSet(viewsets.ModelViewSet):
+    queryset = Donation.objects.all()
+    serializer_class = DonationSerializer
